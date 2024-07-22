42e92c316575a8e62aa0c5592c30bea98a2cb741
James Cacapit <jamescacapit@gmail.com>
2024-07-22 11:06:32 -0700

    github fixes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-//import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,10 +8,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  /*resolve: {
+  resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },*/
+  },
   base: '/VueProject/'
 })
