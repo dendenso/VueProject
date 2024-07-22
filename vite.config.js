422aeb95ba90d0321e6c73dfb16cf81a5e11e449
James Cacapit <jamescacapit@gmail.com>
2024-07-22 10:34:55 -0700

    changed to sub directory

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/VueProject/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
