72b5248d09dda76024ded371e382e287cdf2116d
James Cacapit <jamescacapit@gmail.com>
2024-07-22 10:38:38 -0700

    changed config to a slash

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VueProject/',
+  base: '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
