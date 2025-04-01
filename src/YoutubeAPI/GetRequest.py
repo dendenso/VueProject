0562dfeb777a45a9463e4830e63c90b33a10f610
James Cacapit <jamescacapit@gmail.com>
2025-03-31 20:44:22 -0700

    retrieve youtube channelid and videoid to use url

--- src/YoutubeAPI/GetRequest.py
+++ src/YoutubeAPI/GetRequest.py
@@ -5,7 +5,27 @@ API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 youtube = build("youtube", "v3", developerKey=API_KEY)
 
-def retrieve_channel_id(search_item):
+
+def get_channel_id(search_item):
+      try:
+          """get channel ID"""
+          request = youtube.search().list(
+               part = "snippet",
+               q= search_item,
+               type = "video"
+          )
+          response = request.execute()
+          return response["items"][0]["snippet"]["channelId"]
+      except:
+          print("there was an error")
+
+
+def make_youtube_url(videoId, channelId):
+     return f"https://youtube.com/watch?v={videoId}&ab_channel={channelId}"
+
+
+
+def retrieve_video_id(search_item):
     try:
           """for retrieving the if x is streaming"""
           request = youtube.search().list(
@@ -17,10 +37,11 @@ def retrieve_channel_id(search_item):
 
           )
           response = request.execute()
-          return response["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+          return response["items"][0]["id"]["videoId"]
     except:
           print("does not exist")
+
 # Example usage
-video_q = "Koseki Bijou"  # Replace with any YouTube video ID
+video_q = "Kaminari Clara"  # Replace with any YouTube video ID
 video_info = retrieve_channel_id(video_q)
 
